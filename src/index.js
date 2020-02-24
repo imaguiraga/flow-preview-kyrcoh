81bf7c008d8c705f75ee2ba6f996d01928d4979b
imaguiraga <imaguiraga@gmail.com>
2020-02-23 19:43:41 -0500

    3 files modified, 1 added, 1 deleted

--- src/index.js
+++ src/index.js
@@ -2,46 +2,23 @@ import "./styles.css";
 // using ES6 modules
 import Split from "split.js";
 let instance = null;
-/*
-instance = new Split(["#one", "#two"], {
-  sizes: [25, 75],
-  minSize: [300, 500]
-});
-//*/
-/*
-instance = Split(["#one", "#two"], {
-  direction: "horizontal"
-});
-//*/
-/*
-instance = Split(["#one", "#two"], {
-  snapOffset: 0
-});
-//*/
-/*
-  // "flow-diagram-kyrcoh":"https://github.com/imaguiraga/flow-diagram-kyrcoh.git"*/
+
 Split(["#one", "#two"], {
   sizes: [75, 25],
   //direction: "vertical",
-  minSize: [300, 500],
+  minSize: [300, 400],
   gutter: function(index, direction) {
     var gutter = document.createElement("div");
     gutter.className = "gutter gutter-" + direction;
     //gutter.style.height = "185px";
     return gutter;
   },
-  gutterSize: 8,
-  elementStyle: function(dimension, size, gutterSize) {
-    console.log(dimension);
-    return {
-      "flex-basis": "calc(" + size + "% - " + gutterSize + "px)"
-    };
-  },
-  gutterStyle: function(dimension, gutterSize) {
-    console.log(dimension);
-    let style = {};
-    style["flex-basis"] = gutterSize + "px";
-    return style;
-  }
+  gutterSize: 2,
+  elementStyle: (dimension, size, gutterSize) => ({
+        'flex-basis': `calc(${size}% - ${gutterSize}px)`,
+    }),
+    gutterStyle: (dimension, gutterSize) => ({
+        'flex-basis':  `${gutterSize}px`,
+    })
 });
 console.log(instance);
