08a3c54377323057a8882f8214d2016b1fc77f80
imaguiraga <imaguiraga@gmail.com>
2020-02-25 17:39:43 -0500

    1 file modified

--- src/editor-pane.js
+++ src/editor-pane.js
@@ -1,9 +1,36 @@
 import CodeMirror from "codemirror";
 import "codemirror/lib/codemirror.css";
 import "codemirror/mode/javascript/javascript.js";
+import "codemirror/addon/display/panel.js";
 
 export function createEditor(editor, content, callback){
   return new EditorWrapper(editor, content, callback);
+}
+
+const panels = {};
+let numPanels = 0;
+function makePanel(where) {
+  var node = document.createElement("div");
+  var id = ++numPanels;
+  var widget, close, label;
+
+  node.id = "panel-" + id;
+  node.className = "panel " + where;
+  close = node.appendChild(document.createElement("a"));
+  close.setAttribute("title", "Remove me!");
+  close.setAttribute("class", "remove-panel");
+  close.textContent = "✖";
+  CodeMirror.on(close, "mousedown", function(e) {
+    e.preventDefault()
+    panels[node.id].clear();
+  });
+  label = node.appendChild(document.createElement("span"));
+  label.textContent = "I'm panel n°" + id;
+  return node;
+}
+function addPanel(where,editor) {
+  var node = makePanel(where);
+  panels[node.id] = editor.addPanel(node, {position: where, stable: true});
 }
 
 class EditorWrapper {
@@ -23,6 +50,8 @@ class EditorWrapper {
     if(callback){
       this._cm.on("changes",callback);
     }
+
+    //addPanel("bottom",this._cm);
   }
 
   getContent(){
