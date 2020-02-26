c4b714aca0874867889d3b313572b288e2b69919
imaguiraga <imaguiraga@gmail.com>
2020-02-26 00:13:16 -0500

    1 file added

---
+++ src/bak.js
@@ -0,0 +1,21 @@
+export const content = 
+`let selectClause = () => sequence(a, b, repeat(optional("c")), zeroOrMore("d"));
+let fromClause = () => choice("1", "2", selectClause, "4");
+
+let testflow = choice(
+  terminal("a"),
+  choice("e", "d"),
+  sequence(terminal("b"), terminal("c"),sequence("c","d")),
+  sequence("c","d")
+);
+//*/`;
+
+export const header = 
+`const {
+  repeat,
+  sequence,
+  optional,
+  choice,
+  zeroOrMore,
+  terminal
+} = module;`;
