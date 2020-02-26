cb9ecb8f5d57cd3a3919d677c90c1a917a47d455
imaguiraga <imaguiraga@gmail.com>
2020-02-26 01:43:47 -0500

    1 file added

---
+++ src/flow-util.js
@@ -0,0 +1,35 @@
+import {esprima} from 'esprima';
+var esprima = require('esprima');
+var escodegen = require("escodegen");
+
+export function parseFlow(input){
+  // Parse text
+  var tree = esprima.parseScript(input);
+  var rtree = esprima.parseScript(r);
+  // Modify AST
+  tree.body.push(rtree);
+  let ids = tree.body.filter(
+    (elt) => {return elt.type === 'VariableDeclaration'})
+      .map((elt) => {
+          let id = elt.declarations[0].id.name;
+          return `result.set('${id}',${id});`;
+      });
+      
+  let text =
+    `const {
+      repeat,
+      sequence,
+      optional,
+      choice,
+      zeroOrMore,
+      terminal
+    } = module;
+    ${input}
+    let result = new Map();
+    ${ids.join("\n")}
+    return result;
+  `;
+  console.log(text);
+  //let f = new Function("module",text);
+  //return f();
+}
