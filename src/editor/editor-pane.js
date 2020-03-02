c4d1d622011b55aa406a4428a6aa0f77f06f457b
imaguiraga <imaguiraga@gmail.com>
2020-03-02 13:06:27 -0500

    1 file modified

--- src/editor/editor-pane.js
+++ src/editor/editor-pane.js
@@ -1,11 +1,11 @@
-import "jshint";
+import "jshint/dist/jshint.js";
 import CodeMirror from "codemirror";
 import "codemirror/lib/codemirror.css";
 import "codemirror/mode/javascript/javascript.js";
 import "codemirror/addon/display/panel.js";
 import "codemirror/addon/lint/lint.js";
 import "codemirror/addon/lint/json-lint.js";
-
+import "codemirror/addon/lint/javascript-lint.js";
 
 export function createEditor(editor, content, callback){
   return new EditorWrapper(editor, content, callback);
