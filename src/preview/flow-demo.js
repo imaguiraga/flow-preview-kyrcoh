cc8e25a1d594b3c70d7cf6b8f01fb0e27b7691ff
imaguiraga <imaguiraga@gmail.com>
2020-03-02 07:16:15 -0500

    1 file modified

--- src/preview/flow-demo.js
+++ src/preview/flow-demo.js
@@ -46,40 +46,12 @@ try {
 }catch(e){
   console.error(e.name + ': ' + e.message);
 }
-/*
-let selectClause = () => {
-  return sequence(a, b, repeat(optional("c")), ZeroOrMore("d"));
-};
-//*/
 
-/*
-let selectClause = function() {
-  return sequence(a, b, repeat(optional("c")));
-};
-//*/
-
-function a() {
-  return new TerminalElt("a");
-}
-
-function b() {
-  return new TerminalElt("b");
-}
-
-let testf = new Function('return choice("1", "2", selectClause, "4");');
-//let testflow = choice(terminal("a"), choice("e", "d"));
-//let testflow = choice("e", "d");
-//let testflow = sequence("b", "c");
-// testflow = repeat(terminal("b"));
-// testflow = fromClause();
-console.log(testflow);
 const visitor = new G6Visitor();
 const uidvisitor = new UIDVisitor();
 testflow = uidvisitor.visit(testflow);
 const data = visitor.visit(testflow);
-console.log(JSON.stringify(data));
 
 let graph = diagram.createFlowGraph("container");
 graph.data(data);
-console.log(graph);
 graph.render();
